37ca8d0c8ed839377e3325cee3be3ac735dd643c
Hugo Paul <hugo.paul@hotmail.com>
2024-06-15 14:34:40 -0300

    api pagamentos

--- src/main/java/br/com/solidtechsolutions/apipagamentos/models/Produto.java
+++ src/main/java/br/com/solidtechsolutions/apipagamentos/models/Produto.java
@@ -4,12 +4,63 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
-@RequiredArgsConstructor
-@Data
-@ToString
+
 public class Produto {
     private int id;
     private String name;
     private String price;
     private String description;
+
+    public Produto(int id, String name, String price, String description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public Produto() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price='" + price + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
 }
