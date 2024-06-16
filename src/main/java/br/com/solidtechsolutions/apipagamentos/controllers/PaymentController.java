cf023fcffd80e04adc71837d2d2e391736b1133d
Hugo Paul <hugo.paul@hotmail.com>
2024-06-16 16:54:01 -0300

    cors config

--- src/main/java/br/com/solidtechsolutions/apipagamentos/controllers/PaymentController.java
+++ src/main/java/br/com/solidtechsolutions/apipagamentos/controllers/PaymentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/payments")
 @RestController
 @Slf4j
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://172.233.25.92:5001")
 public class PaymentController {
 
     @Autowired
