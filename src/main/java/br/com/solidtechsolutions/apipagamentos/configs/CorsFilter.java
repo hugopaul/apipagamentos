a5026175fdf1a8ddac01bf8c542a96a817f220ea
Hugo Paul <hugo.paul@hotmail.com>
2024-06-16 15:51:50 -0300

    cors config

---
+++ src/main/java/br/com/solidtechsolutions/apipagamentos/configs/CorsFilter.java
@@ -0,0 +1,45 @@
+package br.com.solidtechsolutions.apipagamentos.configs;
+
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class CorsFilter implements Filter {
+
+    private static final Logger logger = Logger.getLogger(CorsFilter.class.getName());
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String origin = httpRequest.getHeader("Origin");
+
+        if (origin != null && !isOriginAllowed(origin)) {
+            logger.warning("CORS request from origin " + origin + " is not allowed.");
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    private boolean isOriginAllowed(String origin) {
+        // Verifique se a origem é permitida
+        // Substitua pela lógica que você usa para determinar se a origem é permitida
+        return origin.equals("http://172.233.25.92:5001");
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
--- src/main/java/br/com/solidtechsolutions/apipagamentos/configs/SecurityConfiguration.java
+++ src/main/java/br/com/solidtechsolutions/apipagamentos/configs/SecurityConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.servlet.Filter;
+
 @Configuration
 public class SecurityConfiguration implements WebMvcConfigurer {
 
@@ -15,6 +17,11 @@ public class SecurityConfiguration implements WebMvcConfigurer {
                 .allowedOrigins("http://172.233.25.92:5001/**")
                 .allowedHeaders("*")
                 .allowedMethods( "POST");
+    }
+
+    @Bean
+    public Filter corsFilter() {
+        return new CorsFilter();
     }
 
 }
